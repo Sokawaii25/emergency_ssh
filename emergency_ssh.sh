ed12cfce0cad19605f90b981c3aca2208a876005
Sokawaii <anthony.hornec@outlook.fr>
2022-01-22 14:59:34 +0100

    [Dev] Mise en place partie SSH + début ngrok

--- emergency_ssh.sh
+++ emergency_ssh.sh
@@ -14,4 +14,34 @@
 # 3.2) Configurer l'envoi de mail
 # 4) Envoi d'un mail de confirmation
 
+# VARIABLES
+ngrok_directory="~/bin/"
 
+
+# check ssh server
+if [ -f /etc/ssh/sshd_config ]; then
+    echo "Un serveur SSH est déjà installé !"
+else
+    echo "Aucun serveur SSH détecté !"
+    echo "Téléchargement du serveur SSH..."
+    sudo apt-get install openssh-server
+    echo "Serveur SSH installé !"
+    # setup temp key
+    ssh-keygen -f ssh_temp_key -t rsa -N ""
+    # copy key to authorized_keys
+    sudo cp ssh_temp_key.pub /etc/ssh/authorized_keys
+    echo "Clé publique ajoutée au serveur SSH !"
+fi
+
+# download ngrok
+if [ ! -f $ngrok_directory/ngrok ]; then
+    cd $ngrok_directory
+    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+    unzip ngrok-stable-linux-amd64.zip
+    rm ngrok-stable-linux-amd64.zip
+fi
+
+
+# after sending the mail : 
+# remove temp key
+sudo rm ssh_temp_key*
